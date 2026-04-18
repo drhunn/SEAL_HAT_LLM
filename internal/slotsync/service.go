74158b71c225d21393988b84a0ad5b51bb053de3
DANNY HUNN <adfluintia@gmail.com>
2026-04-18 09:32:04 -0500

    Add Go slot sync service

---
+++ internal/slotsync/service.go
@@ -0,0 +1,27 @@
+package slotsync
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/drhunn/LLM-plus-harness/internal/slots"
+)
+
+type Service struct {
+	loader *slots.FilesystemLoader
+	logger *slog.Logger
+}
+
+func NewService(loader *slots.FilesystemLoader, logger *slog.Logger) *Service {
+	return &Service{loader: loader, logger: logger}
+}
+
+func (s *Service) SyncFilesystemView(ctx context.Context, specialistID string) error {
+	files, err := s.loader.LoadSpecialistSlots(specialistID)
+	if err != nil {
+		return err
+	}
+
+	s.logger.InfoContext(ctx, "slot sync snapshot", "specialist_id", specialistID, "slot_count", len(files))
+	return nil
+}
