3c87baca0f702000db3d3ce9b9d197f3709d44a3
DANNY HUNN <adfluintia@gmail.com>
2026-04-18 10:30:38 -0500

    Export new HAT training helpers

--- python/hat_llm/__init__.py
+++ python/hat_llm/__init__.py
@@ -2,6 +2,8 @@
 
 from .config import HatConfig
 from .dataset import DatasetBuilder
+from .hf_dataset import dataset_dict_from_splits, save_dataset_dict
+from .negative_examples import generate_governance_pressure_tasks
 from .policy import HarnessPolicy
 from .postgres_loader import PostgresCorpusLoader
 from .repo_loader import RepositoryLoader
@@ -19,4 +21,7 @@ __all__ = [
     "SlotBundle",
     "TaskExample",
     "TrainingExample",
+    "dataset_dict_from_splits",
+    "save_dataset_dict",
+    "generate_governance_pressure_tasks",
 ]
